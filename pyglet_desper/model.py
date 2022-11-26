f2621983952caf2dce83a3c9711b4e1a5bc1ea4c
Francesco <francesco.mistri98@gmail.com>
2022-11-26 16:34:33 +0100

    Added ImageFileHandle
    Pyglet image loading encapsulated into the Handle abstraction. Images are (by default) atlased and cached in a module level dictionary. This mimics pyglet.resource way of loading images, but in a desper styled class.

--- pyglet_desper/model.py
+++ pyglet_desper/model.py
@@ -3,9 +3,38 @@
 In particular, a set of specialized :class:`desper.Handle`s are
 provided.
 """
+import os.path as pt
+
 import desper
 import pyglet
 from pyglet.media.codecs import MediaDecoder
+from pyglet.image.codecs import ImageDecoder
+from pyglet.image.atlas import TextureBin
+
+
+default_texture_bin = pyglet.image.atlas.TextureBin()
+"""Default texture atlas for :class:`ImageFileHandle`.
+
+All images loaded with said handle class will by default be added
+to an atlas in this bin, which will result in optimized batching
+and hance rendering.
+
+Before loading any images, it is possible to modify the bin's
+:attr:`TextureBin.texture_width` and :attr:`TextureBin.texture_height`
+in order to alter the size of generated atlases (defaults to 2048x2048).
+
+Replacing the bin entirely with a new instance will sort no effect.
+Specify a bin manually as parameter for :class:`ImageFileHandle`
+in that case.
+"""
+
+_image_cache: dict[str, pyglet.image.AbstractImage] = {}
+"""Cache for internal use.
+
+Map absolute filenames to pyglet images. Mainly populated by
+:class:`ImageFileHandle` to prevent reloading the same image multiple
+times.
+"""
 
 
 class MediaFileHandle(desper.Handle[pyglet.media.Source]):
@@ -35,3 +64,51 @@ class MediaFileHandle(desper.Handle[pyglet.media.Source]):
         """Load file with given parameters."""
         return pyglet.media.load(self.filename, streaming=self.streaming,
                                  decoder=self.decoder)
+
+
+class ImageFileHandle(desper.Handle[pyglet.image.AbstractImage]):
+    """Specialized handle for :class:`pyglet.image.AbstractImage`.
+
+    Given a filename (path string), the :meth:`load` implementation
+    tries to load given file as a :class:`pyglet.image.AbstractImage`
+    object.
+
+    By default images are cached and loaded into atlases
+    (:class:`pyglet.image.atlas.TextureAtlas`). This behaviour can be
+    altered through ``atlas``, ``border`` and ``texture_bin``
+    parameters.
+
+    Note that such atlas related parameters are ignored if the image
+    is found in the local cache, as the cached value will be
+    directly returned independently from the given parameters.
+
+    A decoder can be specified. Available
+    decoders can be inspected through
+    :func:`pyglet.image.codecs.get_codecs`.
+    If not specified, the first available codec that supports the given
+    file format will be used.
+    """
+
+    def __init__(self, filename: str,
+                 atlas=True, border: int = 1,
+                 texture_bin: TextureBin = default_texture_bin,
+                 decoder: ImageDecoder = None):
+        self.filename = filename
+        self.atlas = atlas
+        self.border = border
+        self.texture_bin = texture_bin
+        self.decoder = decoder
+
+    def load(self) -> pyglet.image.AbstractImage:
+        """Load file with given parameters."""
+        abs_filename = pt.abspath(self.filename)
+        if abs_filename in _image_cache:
+            return _image_cache[abs_filename]
+
+        image = pyglet.image.load(abs_filename, decoder=self.decoder)
+
+        if self.atlas:
+            image = self.texture_bin.add(image)
+
+        _image_cache[abs_filename] = image
+        return image
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,14 +1,30 @@
 from context import pyglet_desper as pdesper
 
+import os
+import os.path as pt
+
 import pytest
 import pyglet
 
 from helpers import *       # NOQA
 
+pyglet.resource.path = [pt.abspath(os.curdir).replace(os.sep, '/')]
+pyglet.resource.reindex()
+
 
 @pytest.fixture
 def wav_filename():
     return get_filename('files', 'fake_project', 'media', 'yayuh.wav')
+
+
+@pytest.fixture
+def png_filename():
+    return get_filename('files', 'fake_project', 'image', 'logo.png')
+
+
+@pytest.fixture
+def texture_bin():
+    return pyglet.image.atlas.TextureBin()
 
 
 class TestMediaFileHandle:
@@ -24,3 +40,36 @@ class TestMediaFileHandle:
         source = handle.load()
 
         assert isinstance(source, pyglet.media.Source)
+
+
+class TestImageFileHandle:
+
+    def test_init(self, png_filename, texture_bin):
+        handle = pdesper.ImageFileHandle(png_filename, False, 10, texture_bin,
+                                         None)
+
+        assert handle.filename is png_filename
+        assert not handle.atlas
+        assert handle.border == 10
+        assert handle.texture_bin is texture_bin
+        assert handle.decoder is None
+
+    def test_load(self, png_filename, texture_bin):
+        handle = pdesper.ImageFileHandle(png_filename, texture_bin=texture_bin)
+        image = handle.load()
+
+        assert isinstance(image, pyglet.image.AbstractImage)
+        assert pt.abspath(png_filename) in pdesper.model._image_cache
+        assert texture_bin.atlases
+
+        # Test caching
+        handle2 = pdesper.ImageFileHandle(
+            png_filename, texture_bin=texture_bin)
+        assert handle2() is image
+
+    def test_load_no_atlas(self, png_filename, texture_bin):
+        handle = pdesper.ImageFileHandle(png_filename, atlas=False,
+                                         texture_bin=texture_bin)
+        handle.load()
+
+        assert not texture_bin.atlases
